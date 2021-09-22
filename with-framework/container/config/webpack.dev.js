7b57238652b6cb6cf5d6416415d88647c23976a6
Hansmu <hans.miikael@gmail.com>
2021-09-22 10:11:38 +0300

    Add production build configuration for container and marketing

--- with-framework/container/config/webpack.dev.js
+++ with-framework/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const {merge} = require('webpack-merge'); // allows us to merge Webpack configs.
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -20,9 +19,6 @@ const devConfig = {
             },
             shared: packageJson.dependencies // If you want to be very specific about the modules that you are sharing,
             // then you might not necessarily want to do this, but usually this is a nice simplification.
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 };
