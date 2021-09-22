35991b3310f7e310121fc50da56fab289644869a
Hansmu <hans.miikael@gmail.com>
2021-09-22 09:21:36 +0300

    Add automatic module sharing

--- with-framework/marketing/config/webpack.dev.js
+++ with-framework/marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const {merge} = require('webpack-merge'); // allows us to merge Webpack configs.
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -18,7 +19,8 @@ const devConfig = {
             exposes: {
                 './MarketingApp': './src/app/index' // Exporting mount, because we're trying to be library agnostic
             },
-            shared: ['react', 'react-dom']
+            shared: packageJson.dependencies // If you want to be very specific about the modules that you are sharing,
+            // then you might not necessarily want to do this, but usually this is a nice simplification.
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
